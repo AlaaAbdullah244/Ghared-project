db096e8468ca6473eb2eedbef5fb9d1e76ce8a1e
bainamagdy <bainamagdy8@gmail.com>
2026-01-04 23:41:32 +0200

    fix: correct formatting of sender email in sendEmail function

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -16,7 +16,7 @@ const sendEmail = async (options) => {
   });
 
   const mailOptions = {
-    from: `baina <bainamagdy8@gmail.com>`,
+    from: `baina<bainamagdy8@gmail.com>`,
     to: options.email,
     subject: options.subject,
     text: options.message,
